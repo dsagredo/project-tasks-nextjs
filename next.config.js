aabd588c1406b10d3e379c39eec4254dfbb2cfbf
Diego Sagredo <dsagredog@gmail.com>
2023-12-27 08:39:56 -0300

    feat(dashboard): add next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: true,
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {
