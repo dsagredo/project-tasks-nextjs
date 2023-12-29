c5037c8ee04cb5f5a6353b27e21a76488d0ca70e
Diego Sagredo <dsagredog@gmail.com>
2023-12-29 19:37:26 -0300

    chore(tool): config package.json

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,10 +3,6 @@ import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 
 export async function GET(_request: Request) {
-    /* delete * from tasks */
-    await prisma.user.deleteMany();
-    await prisma.tasks.deleteMany();
-
     await prisma.user.create({
         data: {
             email: 'demo@example.com',
@@ -23,23 +19,6 @@ export async function GET(_request: Request) {
             },
         },
     });
-
-    // await prisma.tasks.createMany({
-    //     data: [
-    //         { description: 'Piedra del alma', complete: true },
-    //         { description: 'Piedra del poder' },
-    //         { description: 'Piedra del tiempo' },
-    //         { description: 'Piedra del espacio' },
-    //         { description: 'Piedra del realidad' },
-    //     ],
-    // });
-
-    /*await prisma.tasks.create({
-        data: {
-            description: 'Piedra del alma',
-            complete: true,
-        },
-    });*/
 
     return NextResponse.json({ message: 'Exito database' });
 }
